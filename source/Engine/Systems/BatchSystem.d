0dff018ca149d726fea41aa3706d0a6b51e2df99
Vova616 <Vova616@gmail.com>
2013-11-28 08:21:30 +0200

    BatchSystem Placeholder.

---
+++ source/Engine/Systems/BatchSystem.d
@@ -0,0 +1,12 @@
+module Engine.Systems.BatchSystem;
+
+import Engine.System;
+import Engine.Component;
+import Engine.Entity;
+
+import Engine.Systems.SimpleSystem;
+
+class BatchSystem : SimpleSystem {
+
+	
+}

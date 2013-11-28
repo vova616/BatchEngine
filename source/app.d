1ac3c69fc55d44c529a91c1741a733c8d17bfa33
Vovchik <vova616@gmail.com>
2013-11-28 13:03:34 +0200

    Cleaning app.d.

--- source/app.d
+++ source/app.d
@@ -10,63 +10,12 @@ import Engine.Material;
 import Engine.UI.all;
 import std.math;
 import std.variant;
+import Engine.math;
+
 
 Shader shader;
 Camera camera; 
-
-class System {
-	abstract void start();
-	abstract void process();
-}
-
-struct position {
-	vec3 position;
-	
-	alias position this;
-}
-
-struct position2 {
-	vec3 position;
-
-	alias position this;
-}
-
-
-class CEntity {
-	Variant[] Components;
-}
-
-template CSystem(C...) {
-	class CSystem {
-		
-		bool check(CEntity e) {
-			Continue:
-			foreach(i, c ;C) {
-				foreach(c2 ;e.Components) {
-					if (typeid(c) == c2.type()) {
-						continue Continue;
-					}
-				}
-				return false;
-			}			
-			return true;
-		}
-
-		abstract void start();
-		abstract void process();
-	}
-}
-
-class MovementSystem : CSystem!(position*) {
-	
-	override void start() {
-		
-	}
-
-	override void process() {
-
-	}
-}
+Texture ballTexture;
 
 
 struct GravityMouse  {
@@ -170,48 +119,7 @@ class InputHandle : Component {
 	}
 }
 
-
-import Engine.math;
-
-class A {
-
-	dirty!(vec3) position;
-}
-
-struct B {
-	this(int b) {
-
-	}
-}
-
 void main(string[] args) {
-
-	A a = new A();
-	
-	a.position = vec3(50,0,0);
-	a.position.dirty = false;
-	auto v = a.position.x;
-	assert(a.position.dirty == false);
-	a.position.dirty = false;
-	a.position.normalize();
-	assert(a.position.dirty == true);
-	a.position.dirty = false;
-	a.position.x = 10;
-	assert(a.position.dirty == true);
-	a.position.dirty = false;
-	a.position.update(vec3(0,0,0));
-	assert(a.position.dirty == true);
-	a.position.dirty = false;
-	a.position += a.position;
-	assert(a.position.dirty == true);
-
-	dirty!int b = 10;
-	b++;
-	assert(b.dirty == true);
-	b.dirty = false;
-	b += 3;
-	assert(b.dirty == true);
-
 	try {
 		run();
 	}
@@ -221,38 +129,16 @@ void main(string[] args) {
 	}
 }
 
-Texture ballTexture;
-
 void run() {
 	Core.Start();
-	
-	auto ct = new CEntity();
-	ct.Components.length++;
-	ct.Components[0] = new position(vec3(10,0,0));
-	auto d = new MovementSystem();
-	assert(d.check(ct));
-	d.start();
-	
 
 	Font t = new Font("./public/arial.ttf\0", 32, Font.ASCII);
 	ballTexture = new Texture("./public/sprite.png\0");
 	ballTexture.SetFiltering(GL_LINEAR,GL_LINEAR);
-	//for (int i=0;i<10000;i++) {
-	auto e = new Entity();
-	e.AddComponent!(Sprite)(t.Atlas);
-	//Core.AddEntity(e);
-	e.transform.scale.x = 500;
-	e.transform.scale.y = 500;
-	e.transform.position = vec3(200,400,0);
-	
-	e.transform.scale = vec3(t.Atlas.width, t.Atlas.height, 1);
-	//}
-	
-	//batch.transform.scale = vec3(ballTexture.width, ballTexture.height, 1);
-	
+
+
 	auto mmouse = new Entity();
-	//mmouse.AddComponent(new Sprite(ballTexture));
-	//Core.AddEntity(mmouse);
+	//mmouse.AddComponent!(Sprite)(ballTexture);
 	mmouse.transform.scale = vec3(100, 100, 1);
 	
 			
@@ -260,16 +146,12 @@ void run() {
 		auto ship = new Entity();
 		ship.AddComponent!(Sprite)(ballTexture);
 		ship.AddComponent!(GravityMouse)();
-
-		//ship.AddComponent(new GameOfLife());
 		Core.AddEntity(ship);
 		ship.transform.scale.x = 10;
 		ship.transform.scale.y = 10;
 		ship.transform.rotation = vec3(0,0,0);
 		ship.name = to!string(i);
 		ship.transform.position = vec3((i*2)%Core.width,((i/Core.width)*10)%Core.height,0);
-		//ship.transform.position += vec3(0,10000,0);
-		//ship.transform.position = vec3(uniform(0,Core.width),uniform(0,Core.height),0);
 		ship.transform.scale = vec3(4, 4, 1);
 	}
 	
@@ -283,15 +165,6 @@ void run() {
 		e2.AddComponent!Label(t,to!string(i));
 		Core.AddEntity(e2);	
 	}
-
-	auto e2 = new Entity();
-	e2.transform.scale.x = 32;
-	e2.transform.scale.y = 32;
-	e2.AddComponent!GravityMouse();
-	e2.transform.position = vec3(200,400,0);
-	e2.AddComponent!Label(t,"Hallo FCUKER");
-	//Core.AddEntity(e2);	
-
 
 	auto cam = new Entity();
 	camera = cam.AddComponent(new Camera());
@@ -330,16 +203,18 @@ void run() {
 	});
 	/*
 	StartCoroutine( {
-		auto sprite = ship.GetComponent!Sprite();
-		auto dTexture = new DynamicTexture(sprite.texture);
+		auto sprite = mmouse.GetComponent!Sprite();
+		auto dTexture = new DynamicTexture(sprite.material.texture);
 		float t = 0;
 		while (true) {
 			if (sprite.color.r > 1)
 				sprite.color.r = 0;
-			
-			dTexture.Update!(Vector!(ubyte,4))((pixels){
+
+			alias Vector!(ubyte,4) color;
+
+			dTexture.Update!(color)((pixels){
 				for (int i=0;i<pixels.length;i++) {
-					pixels[i].r =  cast(ubyte)(i + t);
+					pixels[i].r = cast(ubyte)(i + t);
 					pixels[i].g = cast(ubyte)(i + t);
 					pixels[i].b = 0xff;
 					pixels[i].a = 0xff;
@@ -351,9 +226,6 @@ void run() {
 		}
 	});
 	*/
-
-
-	//new Texture("./test3.gif");
 
 	Core.Run();
 }
